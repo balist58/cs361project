7d5e666062855aa7ab26a9b511960dbc5f639177
heinri33 <Heinri33@uwm.edu>
2016-01-27 13:56:43 -0600

    Update tutorial.java

--- cs361Project/src/cs361Project/tutorial.java
+++ cs361Project/src/cs361Project/tutorial.java
@@ -3,7 +3,7 @@ package cs361Project;
 public class tutorial {
 	
 	public static void main(String[] args){
-		System.out.println("Tutorial.");
+		System.out.println("Tutorial...");
 		System.out.println("This is a new change!  ~NB");
 		System.out.println("Y U Do Dis");
 	}
