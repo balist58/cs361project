0825affd0a5d720821c6d31cd4d03ecc6a4dccdd
balist58 <balist58@ceas-942-mac14.cae.uwm.edu>
2016-02-24 13:39:57 -0600

    .

--- cs361Project/src/lab5/LabFiveRunner.java
+++ cs361Project/src/lab5/LabFiveRunner.java
@@ -4,6 +4,17 @@ public class LabFiveRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Bank b = new Bank();
+		ATM a = new ATM(b);
+		
+		b.add(1234, 6789, 80);
+		b.add(6789, 4321, 60);
+		
+		
+		
+		
+
+
 
 	}
 
