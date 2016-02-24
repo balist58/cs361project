fc34440c03cbf3314b8f9d5696a0cc68583f6240
missiaen <missiaen@ceas-942-mac13.cae.uwm.edu>
2016-02-24 13:52:46 -0600

    Added more stuff to the atm class

--- cs361Project/src/lab5/ATM.java
+++ cs361Project/src/lab5/ATM.java
@@ -3,20 +3,44 @@ package lab5;
 public class ATM 
 {
 	Bank bank;
+	boolean isStarted;
+	Bank.Account acnt;
+	
 	public ATM(Bank b) 
 	{
 		bank = b;
+		isStarted = false;
 	}
 	
 	public String start()
 	{
+		isStarted = true;
 		return "ATM Started";
 	}
 	
 	public boolean getAccount(int account, int pin)
 	{
-		return bank.validate(account, pin);
+		if (bank.validate(account, pin) && isStarted) {
+			acnt = bank.getAccount(account);
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
-	
+	public boolean doAction(char action, double amount) 
+	{
+		boolean result = false;
+		if(action == 'W') {
+			
+		} else if(action == 'D') {
+			
+		}
+		
+
+		System.out.print("Current Balance: " + acnt.getBal());
+		acnt = null;
+		isStarted = false;
+		return result;
+	}
 }
--- cs361Project/src/lab5/LabFiveRunner.java
+++ cs361Project/src/lab5/LabFiveRunner.java
@@ -5,12 +5,14 @@ public class LabFiveRunner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Bank b = new Bank();
-		ATM a = new ATM(b);
 		
 		b.add(1234, 6789, 80);
 		b.add(6789, 4321, 60);
 		
+		ATM a = new ATM(b);
+		a.start();
 		
+		System.out.print("Enter Account Number: ");
 		
 		
 
