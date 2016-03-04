ffbbbacf65606c34391ed22755fd84e5be171bcb
Matt <balist58@uwm.edu>
2016-03-03 21:05:29 -0600

    cmdInterfaceTest now prompts user to choose how to read commands.

--- cs361Project/src/cs361Project/CmdInterface.java
+++ cs361Project/src/cs361Project/CmdInterface.java
@@ -75,7 +75,7 @@ public class CmdInterface {
 	private void inputFile(){
 		String filename;
 		Scanner sin = new Scanner(System.in);	
-		System.out.print("Enter filepath of text file containing commands: ");
+		System.out.println("Enter filepath of text file containing commands: ");
 		filename = sin.nextLine();
 		sin.close();
 		
--- cs361Project/src/cs361Project/CmdInterfaceTest.java
+++ cs361Project/src/cs361Project/CmdInterfaceTest.java
@@ -1,5 +1,7 @@
 package cs361Project;
 
+import java.util.Scanner;
+
 /**
  * CmdInterfaceTest.java
  * 
@@ -11,9 +13,18 @@ public class CmdInterfaceTest {
 	public static void main(String[] args) {
 		ChronoTimerControl ct = new ChronoTimerControl();
 		
-		CmdInterface test = new CmdInterface(2,ct);
+		Scanner sin1 = new Scanner(System.in);
+		int option = 0;
+		
+		System.out.println("Enter 1 to enter a command via console.");
+		System.out.println("Enter 2 to enter a command via text file.");
+		System.out.println("Enter 3 to exit.");
+		
+		option = Integer.parseInt(sin1.nextLine());
+		CmdInterface test = new CmdInterface(option,ct);
 		
 		test.go();
+		sin1.close();
 	}
 
 }
