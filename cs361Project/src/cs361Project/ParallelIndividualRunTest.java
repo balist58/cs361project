4caed6784535251f0400b48d74338cbf57594b7f
Garrett Missiaen <missiaen@uwm.edu>
2016-04-17 14:45:19 -0500

    Updated Parallel Individual run tests to match the new implementation

--- cs361Project/src/cs361Project/ParallelIndividualRunTest.java
+++ cs361Project/src/cs361Project/ParallelIndividualRunTest.java
@@ -11,86 +11,100 @@ public class ParallelIndividualRunTest
 {
 	ChronoTimerControl ct = new ChronoTimerControl();
 	CmdInterface ctrl = new CmdInterface(1,ct);
+	ChronoTimerSystem system = ct.getSystem();
 	
 	/**
 	 * TC 3.1, 3.2, 3.3, 3.4
 	 */
 	@Test
 	public void testParallelRace() {
-		ctrl.parseLine("OFF", "11:00:00.0");
-		ctrl.parseLine("ON", "11:00:00.0");
-		ctrl.parseLine("CONN GATE 1", "11:00:01.0");
-		ctrl.parseLine("CONN EYE 2", "11:00:02.0");
-		ctrl.parseLine("CONN GATE 3", "11:00:02.1");
-		ctrl.parseLine("CONN EYE 4", "11:00:02.2");
-		ctrl.parseLine("TOGGLE 1", "11:00:03.0");
-		ctrl.parseLine("TOGGLE 2", "11:00:04.0");
-		ctrl.parseLine("TOGGLE 3", "11:00:04.0");
-		ctrl.parseLine("TOGGLE 4", "11:00:04.0");
-		ctrl.parseLine("EVENT PARIND", "11:00:05.0");
-		ctrl.parseLine("NEWRUN", "11:00:06.0");
+		ct.execute("OFF");
+		ct.execute("ON");
+		ct.execute("CONN GATE 1");
+		ct.execute("CONN EYE 2");
+		ct.execute("CONN GATE 3");
+		ct.execute("CONN EYE 4");
+		ct.execute("TOGGLE 1");
+		ct.execute("TOGGLE 2");
+		ct.execute("TOGGLE 3");
+		ct.execute("TOGGLE 4");
+		ct.execute("EVENT PARIND");
+		ct.execute("NEWRUN");
 		
-		ctrl.parseLine("TRIG 1", "11:00:06.1"); //Shouldn't do anything
-		ctrl.parseLine("TRIG 2", "11:00:06.1"); 
+		system.setTime("11:00:06.1");
+		ct.execute("TRIG 1"); //Shouldn't do anything
+		ct.execute("TRIG 2"); 
 
-		ctrl.parseLine("NUM 888", "11:00:07.0");
-		ctrl.parseLine("NUM 777", "11:00:07.0");
-		ctrl.parseLine("NUM 666", "11:00:07.0");
-		ctrl.parseLine("NUM 555", "11:00:07.0");
-		ctrl.parseLine("NUM 444", "11:00:07.0");
+		ct.execute("NUM 888");
+		ct.execute("NUM 777");
+		ct.execute("NUM 666");
+		ct.execute("NUM 555");
+		ct.execute("NUM 444");
+
+		RunPARIND run = (RunPARIND) system.getRun();
 		
-		assertTrue(ct.isOn());
-		assertEquals(1, ct.getRun().getRunNumber());
-		Runner num444 = ct.getNextRunner();
+		assertTrue(ct.isEnabled());
+		assertEquals(1, run.getRunNumber());
+		Runner num444 = run.getwaitingRunners().peek();
 		assertEquals(444, num444.getNumber());
-		assertNull(ct.getActiveRunner());
-		assertNull(ct.getFinishedRunner());
-		
-		ctrl.parseLine("TRIG 1", "11:00:17.0"); //Start 444 in lane 1
+		assertTrue(run.getActive().isEmpty());
+		assertTrue(run.getFinished().isEmpty());
 
-		Runner num555 = ct.getNextRunner();
+		system.setTime("11:00:17.0");
+		ct.execute("TRIG 1"); //Start 444 in lane 1
+
+		Runner num555 = run.getwaitingRunners().peek();
 		assertEquals(555, num555.getNumber());
-		assertSame(num444, ct.getActiveRunner());
+		assertSame(num444, run.getActive().peek());
 		assertEquals("11:00:17.00", num444.getStart());
-		assertNull(ct.getFinishedRunner());
+		assertTrue(run.getFinished().isEmpty());
 
-		ctrl.parseLine("TRIG 3", "11:00:17.0"); //Start 555 in lane 2
+		system.setTime("11:00:17.0");
+		ct.execute("TRIG 3"); //Start 555 in lane 2
 		
-		Runner num666 = ct.getNextRunner();
+		Runner num666 = run.getwaitingRunners().peek();
 		assertEquals(666, num666.getNumber()); 
-		assertSame(num555, ct.getActiveRunner());
-		assertNull(ct.getFinishedRunner());
-		
-		ctrl.parseLine("TRIG 2", "11:00:30.0"); //Finish 444 in lane 1
-		
-		assertSame(num666, ct.getNextRunner()); 
-		assertSame(num555, ct.getActiveRunner());
-		assertSame(num444, ct.getFinishedRunner());
+		assertSame(num555, run.getActive().peekLast());
+		assertTrue(run.getFinished().isEmpty());
 
-		ctrl.parseLine("TRIG 4", "11:00:32.0"); //Finish 555 in lane 2
-		ctrl.parseLine("TRIG 3", "11:00:33.0"); //Start 666 in lane 2
+		system.setTime("11:00:30.0");
+		ct.execute("TRIG 2"); //Finish 444 in lane 1
 		
-		Runner num777 = ct.getNextRunner();
+		assertSame(num666, run.getwaitingRunners().peek()); 
+		assertSame(num555, run.getActive().peekLast());
+		assertSame(num444, run.getFinished().peekLast());
+
+		system.setTime("11:00:32.0");
+		ct.execute("TRIG 4"); //Finish 555 in lane 2
+		system.setTime("11:00:33.0");
+		ct.execute("TRIG 3"); //Start 666 in lane 2
+		
+		Runner num777 = run.getwaitingRunners().peek();
 		assertEquals(777, num777.getNumber()); 
-		assertSame(num666, ct.getActiveRunner());
-		assertSame(num555, ct.getFinishedRunner());
-		
-		ctrl.parseLine("TRIG 1", "11:00:33.0"); //Start 777 in lane 1
-		ctrl.parseLine("TRIG 3", "11:00:35.0"); //Start 888 in lane 2
-		
-		assertNull(ct.getNextRunner());
-		Runner num888 = ct.getActiveRunner();
-		assertEquals(888, num888.getNumber());
-		assertSame(num555, ct.getFinishedRunner());
-		
-		ctrl.parseLine("TRIG 2", "11:00:40.0"); //Finish 777 in lane 1
-		assertSame(num777, ct.getFinishedRunner());
+		assertSame(num666, run.getActive().peek());
+		assertSame(num555, run.getFinished().peekLast());
 
-		ctrl.parseLine("TRIG 4", "11:00:41.0"); //Finish 666 in lane 2
-		assertSame(num666, ct.getFinishedRunner());
+		system.setTime("11:00:33.0");
+		ct.execute("TRIG 1"); //Start 777 in lane 1
+		system.setTime("11:00:35.0");
+		ct.execute("TRIG 3"); //Start 888 in lane 2
 		
-		ctrl.parseLine("TRIG 4", "11:00:43.0"); //Finish 888 in lane 2
-		assertSame(num888, ct.getFinishedRunner());
+		assertTrue(run.getwaitingRunners().isEmpty());
+		Runner num888 = run.getActive().peekLast();
+		assertEquals(888, num888.getNumber());
+		assertSame(num555, run.getFinished().peekLast());
+
+		system.setTime("11:00:40.0");
+		ct.execute("TRIG 2"); //Finish 777 in lane 1
+		assertSame(num777, run.getFinished().peekLast());
+
+		system.setTime("11:00:41.0");
+		ct.execute("TRIG 4"); //Finish 666 in lane 2
+		assertSame(num666, run.getFinished().peekLast());
+
+		system.setTime("11:00:43.0");
+		ct.execute("TRIG 4"); //Finish 888 in lane 2
+		assertSame(num888, run.getFinished().peekLast());
 		
 		assertEquals("11:00:17.00", num444.getStart());
 		assertEquals("11:00:30.00", num444.getEnd());
@@ -112,12 +126,12 @@ public class ParallelIndividualRunTest
 		assertEquals("11:00:43.00", num888.getEnd());
 		assertEquals("8.0 seconds", num888.getTotalTime());
 		
-		assertEquals(11,ct.getTime().get(10));
-		assertEquals(0,ct.getTime().get(12));
-		assertEquals(43,ct.getTime().get(13));
-		assertEquals(0,ct.getTime().get(14));
+		assertEquals(11,system.getTime().get(10));
+		assertEquals(0,system.getTime().get(12));
+		assertEquals(43,system.getTime().get(13));
+		assertEquals(0,system.getTime().get(14));
 		
-		ct.export(1);
+		system.export(1);
 	}
 }
 
